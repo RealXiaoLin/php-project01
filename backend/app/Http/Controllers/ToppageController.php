cbac569cbad4dcc7f98a67fd274415aad4e6a4f5
RealXiaoLin <yukixechan@gmail.com>
2021-01-06 20:55:38 +0900

    create test view

---
+++ backend/app/Http/Controllers/ToppageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ToppageController extends Controller
+{
+    public function index()
+    {
+      return view('toppages.index');
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -25,10 +25,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+use App\Http\Controllers\ToppageController;
+Route::get('/index',  [ToppageController::class, 'index']);
