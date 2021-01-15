f534dd65ecdc90d262c0e97a43f06122e0f9491b
RealXiaoLin <yukixechan@gmail.com>
2021-01-15 21:23:29 +0900

    add link from workbook to question

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -30,7 +30,7 @@ Route::get('/',  [ToppageController::class, 'index']);
 Route::get('/workbook',  [ToppageController::class, 'workbook'])->middleware('auth');
 
 use App\Http\Controllers\QuestionController;
-Route::resource('/question',  QuestionController::class);
+Route::resource('/question',  QuestionController::class)->middleware('auth');
 
 use App\Http\Controllers\MypageController;
 Route::get('/mypage',  [MypageController::class, 'index'])->middleware('auth');
