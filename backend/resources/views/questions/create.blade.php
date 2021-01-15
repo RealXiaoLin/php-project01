840e16f63f6ea95737dd55058d1c19e0fce93832
RealXiaoLin <yukixechan@gmail.com>
2021-01-15 23:43:54 +0900

    Add error messages about questions create

--- backend/resources/views/questions/create.blade.php
+++ backend/resources/views/questions/create.blade.php
@@ -8,23 +8,41 @@
         <div class="mb-3">
           <p class="bg-dark text-white fw-bold pl-2 pt-2 pb-2 mt-1 mb-0">問題文</p>
           <textarea name="body" rows="5" class="form-control"></textarea>
+          @error('body')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
         </div>
         <div class="mb-3">
           <p class="bg-dark text-white fw-bold pl-2 pt-2 pb-2 mt-1 mb-0">選択肢</p>
           選択肢1<input type="text" name="choice_1" class="form-control">
+          @error('choice_1')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
         </div>
         <div class="mb-3">
           選択肢2<input type="text" name="choice_2" class="form-control">
+          @error('choice_2')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
         </div>
         <div class="mb-3">
           選択肢3<input type="text" name="choice_3" class="form-control">
+          @error('choice_3')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
         </div>
         <div class="mb-3">
           選択肢4<input type="text" name="choice_4" class="form-control">
+          @error('choice_4')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
         </div>
         <div class="mb-3">
           <p class="bg-dark text-white fw-bold pl-2 pt-2 pb-2 mt-1 mb-0">回答文</p>
           <textarea name="answer_body" rows="5" class="form-control"></textarea>
+          @error('answer_body')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
         </div>
         <div class="mb-3">
           <p class="bg-dark text-white fw-bold pl-2 pt-2 pb-2 mt-1 mb-0">正解択</p>
@@ -35,6 +53,9 @@
             <option value="3">選択肢3</option>
             <option value="4">選択肢4</option>
           </select>
+          @error('answer_choice')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
         </div>
         <button type="submit" class="btn btn-primary">投稿する</button>
       </form>
