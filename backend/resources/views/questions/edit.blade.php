fa3a5006e5930ef36b0781f891783b88692d72d7
RealXiaoLin <yukixechan@gmail.com>
2021-01-22 17:13:16 +0900

    Make questions edit pages

--- backend/resources/views/questions/edit.blade.php
+++ backend/resources/views/questions/edit.blade.php
@@ -3,26 +3,75 @@
 <div class="container">
   <div class="question-form row">
     <div class="col-8 mx-auto">
-      <form action="/question" method="POST">
+      <form action="/question/{{$question->id}}" method="POST">
+        @method('PUT')
         @csrf
+        @isset($workbooks)
         <div class="mb-3">
-          @isset($workbooks)
-            <p class="bg-dark text-white fw-bold pl-2 pt-2 pb-2 mt-1 mb-0">フォルダ</p>
-            @foreach ($workbooks as $workbook)
-              <div class="form-check mt-1">
-                <input class="form-check-input" type="radio" value="{{ $workbook->id }}", id="flexCheckDefault" name="workbook_id">
-                <label class="form-check-label" for="flexCheckDefault">
-                  {{ $workbook->title }}
-                </label>
-              </div>
-            @endforeach
-          @endisset
+          <p class="bg-dark text-white fw-bold pl-2 pt-2 pb-2 mt-1 mb-0">フォルダ</p>
+          @foreach ($workbooks as $workbook)
+          <div class="form-check mt-1">
+            <input class="form-check-input" type="radio" value="{{ $workbook->id }}", id="flexCheckDefault" name="workbook_id">
+            <label class="form-check-label" for="flexCheckDefault">
+              {{ $workbook->title }}
+            </label>
+          </div>
+          @endforeach
         </div>
-        @component('components.set-question')
-          @slot('btn_mark')
-            決定
-          @endslot
-        @endcomponent
+        @endisset
+        <div class="mb-3">
+          <p class="bg-dark text-white fw-bold pl-2 pt-2 pb-2 mt-1 mb-0">問題文</p>
+          <textarea name="body" rows="5" class="form-control">{{ old('body') }}{{ $question->body }}</textarea>
+          @error('body')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
+        </div>
+        <div class="mb-3">
+          <p class="bg-dark text-white fw-bold pl-2 pt-2 pb-2 mt-1 mb-0">選択肢</p>
+          選択肢1<input type="text" name="choice_1" class="form-control" value="{{ old('choice_1') }}{{ $question->choice_1 }}">
+          @error('choice_1')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
+        </div>
+        <div class="mb-3">
+          選択肢2<input type="text" name="choice_2" class="form-control" value="{{ old('choice_2') }}{{ $question->choice_2 }}">
+          @error('choice_2')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
+        </div>
+        <div class="mb-3">
+          選択肢3<input type="text" name="choice_3" class="form-control" value="{{ old('choice_3') }}{{ $question->choice_3 }}">
+          @error('choice_3')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
+        </div>
+        <div class="mb-3">
+          選択肢4<input type="text" name="choice_4" class="form-control" value="{{ old('choice_4') }}{{ $question->choice_4 }}">
+          @error('choice_4')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
+        </div>
+        <div class="mb-3">
+          <p class="bg-dark text-white fw-bold pl-2 pt-2 pb-2 mt-1 mb-0">回答文</p>
+          <textarea name="answer_body" rows="5" class="form-control">{{ old('answer_body') }}{{ $question->answer_body }}</textarea>
+          @error('answer_body')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
+        </div>
+        <div class="mb-3">
+          <p class="bg-dark text-white fw-bold pl-2 pt-2 pb-2 mt-1 mb-0">正解択</p>
+          <select name="answer_choice" class="form-control"  value="{{ old('answer_choice') }}">
+            <option selected>正解択を選択してください</option>
+            <option value="1">選択肢1</option>
+            <option value="2">選択肢2</option>
+            <option value="3">選択肢3</option>
+            <option value="4">選択肢4</option>
+          </select>
+          @error('answer_choice')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">編集する</button>
       </form>
     </div>
     <div class="col-4">
